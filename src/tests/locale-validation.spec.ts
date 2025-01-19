748e922aa53549cf073c4073ce490705a48cd75d
Kodo Yousif <kodo.yousif@gmail.com>
2025-01-19 14:07:06 +0300

    adding basic test library with husky configuration

---
+++ src/tests/locale-validation.spec.ts
@@ -0,0 +1,13 @@
+describe('Localization files', () => {
+  it('English Localization', () => {
+    expect(true).toBe(true);
+  });
+
+  it('Arabic Localization', () => {
+    expect(false).toBe(false);
+  });
+
+  it('Kurdish Localization', () => {
+    expect(false).toBe(false);
+  });
+});
