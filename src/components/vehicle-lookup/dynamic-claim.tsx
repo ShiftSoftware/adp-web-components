af13242e544f617f41b62530544078e8f934ca9a
Kodo Yousif <kodo.yousif@gmail.com>
2025-01-23 15:05:24 +0300

    fix dynamic claim button style

--- src/components/vehicle-lookup/dynamic-claim.tsx
+++ src/components/vehicle-lookup/dynamic-claim.tsx
@@ -410,7 +410,7 @@ export class DynamicClaim implements VehicleInformationInterface {
             </table>
 
             {item.status === 'pending' && (
-              <button onClick={() => this.claim(item)} class="claim-button">
+              <button onClick={() => this.claim(item)} class="claim-button dynamic-claim-button">
                 <svg width="30px" height="30px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <g stroke-width="0"></g>
                   <g stroke-linecap="round" stroke-linejoin="round"></g>
