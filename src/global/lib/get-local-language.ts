28edb8bc4f933fc03a5af165f5dad8d534451ba2
Kodo Yousif <kodo.yousif@gmail.com>
2025-01-23 17:09:05 +0300

    use dedicated versioning for language files

--- src/global/lib/get-local-language.ts
+++ src/global/lib/get-local-language.ts
@@ -1,4 +1,5 @@
 import { Build } from '@stencil/core';
+import { version } from '../../../package.json';
 import { languageMapper, Locale } from '~types/locales';
 
 const cachedLocales = {};
@@ -11,7 +12,7 @@ export const getLocaleLanguage = async (fileKey: string): Promise<Locale> => {
   let localeResponse;
 
   if (Build.isDev) localeResponse = fetch('../../locales/' + languageFile).then(res => res.json() as Locale);
-  else localeResponse = fetch('https://cdn.jsdelivr.net/npm/adp-web-components@latest/dist/locales/' + languageFile).then(res => res.json() as Locale);
+  else localeResponse = fetch(`https://cdn.jsdelivr.net/npm/adp-web-components@${version}/dist/locales/${languageFile}`).then(res => res.json() as Locale);
 
   cachedLocales[languageFile] = localeResponse;
 
