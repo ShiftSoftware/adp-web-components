59cbf1b45f9a48f2b2ffddc8a12e4174a5ed38f7
Kodo Yousif <kodo.yousif@gmail.com>
2025-05-12 14:42:05 +0300

    migrate distributor

--- src/components/part-lookup/distributor-lookup.tsx
+++ src/components/part-lookup/distributor-lookup.tsx
@@ -1,13 +1,15 @@
+import { InferType } from 'yup';
 import { Component, Element, Host, Method, Prop, State, Watch, h } from '@stencil/core';
 
 import cn from '~lib/cn';
-import { getLocaleLanguage } from '~lib/get-local-language';
+import { ErrorKeys, getLocaleLanguage, getSharedLocal, SharedLocales, sharedLocalesSchema } from '~lib/get-local-language';
 
+import { LanguageKeys } from '~types/locale';
 import { AppStates, MockJson } from '~types/components';
 import { PartInformation } from '~types/part-information';
-import { ErrorKeys, LanguageKeys, Locale, localeSchema } from '~types/a';
 
 import { getPartInformation, PartInformationInterface } from '~api/partInformation';
+import distributerSchema from '~locales/partLookup/distributor/type';
 
 let mockData: MockJson<PartInformation> = {};
 
@@ -31,7 +33,9 @@ export class DistributorLookup implements PartInformationInterface {
   @State() errorMessage?: ErrorKeys = null;
   @State() partInformation?: PartInformation;
   @State() externalPartNumber?: string = null;
-  @State() locale: Locale = localeSchema.getDefault();
+
+  @State() sharedLocales: SharedLocales = sharedLocalesSchema.getDefault();
+  @State() locale: InferType<typeof distributerSchema> = distributerSchema.getDefault();
 
   abortController: AbortController;
   networkTimeoutRef: ReturnType<typeof setTimeout>;
@@ -44,7 +48,9 @@ export class DistributorLookup implements PartInformationInterface {
 
   @Watch('language')
   async changeLanguage(newLanguage: LanguageKeys) {
-    this.locale = await getLocaleLanguage(newLanguage);
+    const localeResponses = await Promise.all([getLocaleLanguage(newLanguage, 'partLookup.distributor', distributerSchema), getSharedLocal(newLanguage)]);
+    this.locale = localeResponses[0];
+    this.sharedLocales = localeResponses[1];
   }
 
   private handleSettingData(response: PartInformation) {
@@ -119,7 +125,7 @@ export class DistributorLookup implements PartInformationInterface {
   }
 
   render() {
-    const texts = this.locale.partLookup.distributor;
+    const texts = this.locale;
 
     const localName = this.partInformation ? this.localizationName || 'russian' : 'russian';
 
@@ -171,14 +177,14 @@ export class DistributorLookup implements PartInformationInterface {
 
     return (
       <Host>
-        <div dir={this.locale.direction} class="min-h-[100px] relative transition-all duration-300 overflow-hidden">
+        <div dir={this.sharedLocales.direction} class="min-h-[100px] relative transition-all duration-300 overflow-hidden">
           <div>
             <loading-spinner isLoading={this.state.includes('loading')} />
             <div class={cn('transition-all !duration-700', { 'scale-0': this.state.includes('loading') || this.state === 'idle', 'opacity-0': this.state.includes('loading') })}>
               {['error', 'error-loading'].includes(this.state) && (
                 <div class="py-[16px] min-h-[100px] flex items-center">
                   <div class=" px-[16px] py-[8px] border reject-card text-[20px] rounded-[8px] w-fit mx-auto">
-                    {this.locale.errors[this.errorMessage] || this.locale.errors.wildCard}
+                    {this.sharedLocales.errors[this.errorMessage] || this.sharedLocales.errors.wildCard}
                   </div>
                 </div>
               )}
