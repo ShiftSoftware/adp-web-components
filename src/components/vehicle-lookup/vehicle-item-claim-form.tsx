7120d98368f4c172b8ea8f7af256d273f95b2de2
Kodo Yousif <kodo.yousif@gmail.com>
2025-06-15 06:09:46 +0300

    add document button to claimable form qrCode mode

--- src/components/vehicle-lookup/vehicle-item-claim-form.tsx
+++ src/components/vehicle-lookup/vehicle-item-claim-form.tsx
@@ -496,7 +496,62 @@ export class VehicleItemClaimForm {
 
                 <div class={cn('transition-opacity duration-300 ', { 'opacity-50': this.isLoading })}>
                   {this.claimViaBarcodeScanner && (
-                    <div style={{ display: 'flex', flexDirection: 'row', gap: '50px', padding: '0 75px' }}>
+                    <div class="flex flex-col mt-[50px] px-[75px] gap-[16px]">
+                      {this.item?.showDocumentUploader && (
+                        <div class="flex mb-[12px] flex-col">
+                          <input class="document-uploader" disabled={this.isLoading} type="file" accept="image/*,application/pdf" hidden />
+                          <div
+                            class={cn(
+                              'document-button overflow-hidden flex items-center w-fit mx-auto cursor-pointer gap-[16px] ps-[8px] h-[32px] transition duration-300 text-white bg-[#275e8f] active:bg-[#223f57] hover:bg-[#3071a9] rounded-[5px]',
+                              { 'pointer-events-none bg-[#d5d5d5] cursor-default': disableInput },
+                            )}
+                          >
+                            <div title={this.selectedFile?.name || texts.document} class="max-w-[200px] truncate whitespace-nowrap overflow-hidden text-ellipsis">
+                              {this.selectedFile?.name || texts.document}
+                              {this?.item?.documentUploaderIsRequired && !this.selectedFile && <span class={cn('ps-[4px] text-red-500', { 'text-white': disableInput })}>*</span>}
+                            </div>
+                            <button
+                              onClick={this.clearFile}
+                              class={cn('overflow-hidden transition duration-300 hover:bg-red-600 relative cursor-pointer flex items-center justify-center size-[32px]', {
+                                'pointer-events-none cursor-default': !this.selectedFile,
+                              })}
+                            >
+                              <svg
+                                fill="none"
+                                stroke-width="2"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                xmlns="http://www.w3.org/2000/svg"
+                                class={cn('size-[24px] absolute transition duration-500', { '-translate-y-[32px]': !!this.selectedFile })}
+                              >
+                                <path d="m16 6-8.414 8.586a2 2 0 0 0 2.829 2.829l8.414-8.586a4 4 0 1 0-5.657-5.657l-8.379 8.551a6 6 0 1 0 8.485 8.485l8.379-8.551" />
+                              </svg>
+                              <svg
+                                fill="none"
+                                stroke-width="2"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                xmlns="http://www.w3.org/2000/svg"
+                                class={cn('size-[24px] absolute transition translate-y-[32px] duration-500', { 'translate-y-0': !!this.selectedFile })}
+                              >
+                                <path d="M18 6 6 18" />
+                                <path d="m6 6 12 12" />
+                              </svg>
+                            </button>
+                          </div>
+                          <flexible-container isOpened={this.isDocumentError}>
+                            <div class="text-red-700 w-fit mx-auto pt-[8px]">
+                              {this.documentError === 'documentLimitError'
+                                ? texts.documentLimitError + `${this.maximumDocumentFileSizeInMb}Mb`
+                                : texts.documentRequiredError || this.sharedLocales.errors.wildCard}
+                            </div>
+                          </flexible-container>
+                        </div>
+                      )}
                       <input
                         id="qr-input"
                         dir="ltr"
